81459f1c27ea1c092b49dc665a3e099bca541c3d
John S <toxick@gmail.com>
2017-01-23 00:08:04 +0000

    refactored server.js and created passport.js

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 //importing different modules
-var express = require('express'),
-    passport = require('passport'),
-    mongoose = require('mongoose'),
-    LocalStrategy = require('passport-local').Strategy;
+var express = require('express');
 
 //environment variable, to determine if you are in production or development. Setting development as the default if it hasnt been already set
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -19,43 +16,8 @@ require('./server/config/express')(app, config);
 require('./server/config/mongoose')(config);
 
 //Passport
+require('./server/config/passport')();
 
-//grabbing the User model so passport can auth
-var User = mongoose.model('User');
-
-passport.use(new LocalStrategy(
-    function(username, password, done){
-        //find username in User model from username passport and then exec function
-        User.findOne({username:username}).exec(function (err, user) {
-            //checks to see if user exist but is also authenticated
-            //function from user schema
-            if(user && user.authenticate(password)){
-                return done(null, user);
-            } else {
-                return done(null, false);
-            }
-
-        })
-    }
-));
-
-
-//serializeUser determines, which data of the user object should be stored in the session.
-passport.serializeUser(function(user, done){
-    if(user){
-        done(null, user._id);
-    }
-});
-
-passport.deserializeUser(function (id, done) {
-    User.findOne({_id:id}).exec(function (err, user) {
-        if(user){
-            return done(null, user);
-        } else{
-            return done(null, false);
-        }
-    })
-});
 
 //Routing
 require('./server/config/routes')(app);
---
+++ server/config/passport.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    User = mongoose.model('User');
+
+
+module.exports = function() {
+    //grabbing the User model so passport can auth
+    //Using local-passport rules to auth with User DB username and password. 
+    passport.use(new LocalStrategy(
+        function(username, password, done){
+            //find username in User model from username passport and then exec function
+            User.findOne({username:username}).exec(function (err, user) {
+                //checks to see if user exist but is also authenticated
+                //function from user schema
+                if(user && user.authenticate(password)){
+                    return done(null, user);
+                } else {
+                    return done(null, false);
+                }
+
+            })
+        }
+    ));
+
+
+//serializeUser determines, which data of the user object should be stored in the session.
+    passport.serializeUser(function(user, done){
+        if(user){
+            done(null, user._id);
+        }
+    });
+
+    passport.deserializeUser(function (id, done) {
+        User.findOne({_id:id}).exec(function (err, user) {
+            if(user){
+                return done(null, user);
+            } else{
+                return done(null, false);
+            }
+        })
+    });
+};
