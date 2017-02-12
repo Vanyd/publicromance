cff9c58412af4705a64d60708507e0758d961244
John S <toxick@gmail.com>
2017-02-12 01:22:18 +0000

    Added Sorting and filtering on to the Stock List

--- public/app/stock/mvStockListCtrl.js
+++ public/app/stock/mvStockListCtrl.js
@@ -1,4 +1,13 @@
 angular.module('app').controller('mvStockListCtrl',function ($scope, mvProduct) {
    //Get list of Stock items to use for data table from the mvProduct Resouce
     $scope.products = mvProduct.query();
+
+    //
+    $scope.sortOptions = [{value:"title", text: "Sort by Title"},
+        {value:"category", text: "Sort by Category"}];
+
+    //setting sortOrder to default to first sortOption
+    $scope.sortOrder = $scope.sortOptions[0].value;
+
+
 });
